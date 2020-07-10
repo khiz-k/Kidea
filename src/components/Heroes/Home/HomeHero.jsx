5cab486384a7722c8482d504f4c8c5766000262b
Dora He <kamiyajelly@gmail.com>
2020-07-10 14:49:35 -0400

    change intro text for hero

--- src/components/Heroes/Home/HomeHero.jsx
+++ src/components/Heroes/Home/HomeHero.jsx
@@ -7,7 +7,7 @@ export default function HomeHero (props) {
       <section className="homehero">
         <div className='homehero-left'>
           <h1 className="homehero-header">Educational <span className='homehero-important'>Media</span> for <span className="homehero-important">Kids</span> to watch</h1>
-          <p className="homehero-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation  irure.Ut enim ad minim veniam, quis nostrud exercitation</p>
+          <p className="homehero-text">Parents face a really difficult time tracking what their kids are watching. Kidea suggests very specific content that is age-targeted so you don’t have to worry anymore.</p>
           <a href="https://www.instagram.com/explore/tags/kidea/">
           <button className="homehero-button">ABOUT US</button>
 
