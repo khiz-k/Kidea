17693f76d7c1da08dec93a1998b375400aa0eacc
khizark96 <58965282+khizark96@users.noreply.github.com>
2020-07-10 12:55:08 -0700

    Update AboutHero.jsx

--- src/components/Heroes/About/AboutHero.jsx
+++ src/components/Heroes/About/AboutHero.jsx
@@ -4,8 +4,21 @@ import './aboutHero.scss';
 export default function AboutHero (props) {
   return (
     <>
-      <section className="hero">
-   
+       <section className="about-hero">
+        <div className='about-hero-left'>
+          <h1 className="about-hero-header">Team <span className='about-hero-important'>Happy</span> Feet</h1>
+          <p className="about-hero-text">Group of happy-go-lucky tech savvy individuals embracing the idea of .</p>
+        </div>
+        <div className="about-hero-right">
+          <div className="coupled-images">
+          <img src='./assets/Khizar.png'className='about-hero-img1'/>
+          <img src='./assets/Rey.jpg'className='about-hero-img2'/>
+          </div>
+          <div className="coupled-images">
+          <img src='./assets/Doraa.jpg'className='about-hero-img3'/>
+          <img src='./assets/Bonney.png' className='about-hero-img4'/>
+          </div>
+        </div>
       </section>
     </>
   );
