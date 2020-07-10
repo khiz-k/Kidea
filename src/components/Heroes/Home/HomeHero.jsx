3e03b3f48c7e5a561ddc244557beeb2902c961a7
Dora He <kamiyajelly@gmail.com>
2020-07-10 14:46:52 -0400

    add style for home hero and nav bar

--- src/components/Heroes/Home/HomeHero.jsx
+++ src/components/Heroes/Home/HomeHero.jsx
@@ -5,12 +5,19 @@ export default function HomeHero (props) {
   return (
     <>
       <section className="homehero">
-        <div>
-          <h1 className="homehero-header">Educational Media for Kids to watch</h1>
-          <p className="homehero-text">Lorem ipsum</p>
+        <div className='homehero-left'>
+          <h1 className="homehero-header">Educational <span className='homehero-important'>Media</span> for <span className="homehero-important">Kids</span> to watch</h1>
+          <p className="homehero-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation  irure.Ut enim ad minim veniam, quis nostrud exercitation</p>
+          <a href="https://www.instagram.com/explore/tags/kidea/">
           <button className="homehero-button">ABOUT US</button>
+
+          </a>
         </div>
-        <img src={props.image}/>
+        <div className="homehero-right">
+        <img src='./assets/WhimsicalAnimal_08.svg'className='homehero-img1'/>
+        <img src='./assets/WhimsicalAnimal_13.svg'className='homehero-img2'/>
+
+        </div>
       </section>
     </>
   );
