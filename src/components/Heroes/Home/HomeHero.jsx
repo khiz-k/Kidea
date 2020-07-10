8851a03951523e19c51104da39714a5e5736c28c
Dora He <kamiyajelly@gmail.com>
2020-07-10 14:55:28 -0400

    update about us yellow button to link

--- src/components/Heroes/Home/HomeHero.jsx
+++ src/components/Heroes/Home/HomeHero.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './homeHero.scss';
-
+import {Link} from 'react-router-dom';
 export default function HomeHero (props) {
   return (
     <>
@@ -8,10 +8,11 @@ export default function HomeHero (props) {
         <div className='homehero-left'>
           <h1 className="homehero-header">Educational <span className='homehero-important'>Media</span> for <span className="homehero-important">Kids</span> to watch</h1>
           <p className="homehero-text">Parents face a really difficult time tracking what their kids are watching. Kidea suggests very specific content that is age-targeted so you don’t have to worry anymore.</p>
-          <a href="https://www.instagram.com/explore/tags/kidea/">
+          <Link to="/about">
+
           <button className="homehero-button">ABOUT US</button>
 
-          </a>
+          </Link>
         </div>
         <div className="homehero-right">
         <img src='./assets/WhimsicalAnimal_08.svg'className='homehero-img1'/>
